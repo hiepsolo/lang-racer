c4de2417a49deaba230d4ff8b2a31afbe033d150
hhng <hiep.nguyen@optimizely.com>
2023-09-09 17:00:33 +0700

    Add footer UI

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='h-16 flex justify-between items-center w-screen px-4 border border-t-gray-200'>
+      <p className='text-xs text-gray-500'>Copyright &copy; 2023 Hiep Solo</p>
+      <p className='text-xl font-semibold'>Lang Racer</p>
+    </div>
   )
 }
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,11 +3,10 @@ import './Layout.css';
 import Footer from './Footer';
 import Header from './Header';
 import { PropsWithChildren } from 'react';
-import React from 'react';
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='layout w-screen min-h-screen grid grid-rows-[64px_minmax(calc(100vh-64px-100px),_1fr)_100px] h-full'>
+    <div className='layout w-full min-h-screen grid grid-rows-[64px_minmax(calc(100vh-64px-100px),_1fr)_64px] h-full'>
       <Header />
       <div className='container mt-8'>{children}</div>
       <Footer />
